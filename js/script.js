9b4f80396f526204eb6513742710facadd45a577
SekoVper <damehlivingstone1@gmail.com>
2022-10-19 15:44:16 +0100

    Add About Page

--- js/script.js
+++ js/script.js
@@ -4,50 +4,48 @@ const menuContainer = document.querySelector('.menu-bg');
 const socialMenu = document.querySelector('.social-nav');
 const mainMenuContainer = document.querySelector('.menu-only');
 
-
 toggler.addEventListener('click', () => {
   toggler.classList.toggle('active');
   navMenu.classList.toggle('show-menu');
   menuContainer.classList.toggle('show-bg');
   socialMenu.classList.toggle('show-social-nav');
   mainMenuContainer.classList.toggle('menu-only-show');
-})
-
+});
 
 // speakers
-speakers = [
+const speakers = [
   {
     id: 0,
-    name: "Angie Jones",
-    imgUrl: "img/angie-jones.jpg",
-    title: "Consectetur sint, maxime adipisci fuga nobis quo!",
-    bio: "Lorem, ipsum dolor sit amet consectetur adipisicing elit dolort et uhuio",
+    name: 'Angie Jones',
+    imgUrl: 'img/angie-jones.jpg',
+    title: 'Consectetur sint, maxime adipisci fuga nobis quo!',
+    bio: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit dolort et uhuio',
   },
   {
     id: 1,
-    name: "Bjarne Seko",
-    imgUrl: "img/Bjarne.jpg",
-    title: "Consectetur sint, maxime adipisci fuga nobis quo!",
-    bio: "Lorem, ipsum dolor sit amet consectetur adipisicing elit dolort et uhuio",
+    name: 'Bjarne Seko',
+    imgUrl: 'img/Bjarne.jpg',
+    title: 'Consectetur sint, maxime adipisci fuga nobis quo!',
+    bio: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit dolort et uhuio',
   },
   {
     id: 2,
-    name: "Cassie Kozyrkov",
-    imgUrl: "img/Cassie-Kozyrkov.jpg",
-    title: "Consectetur sint, maxime adipisci fuga nobis quo!",
-    bio: "Lorem, ipsum dolor sit amet consectetur adipisicing elit dolort et uhuio",
+    name: 'Cassie Kozyrkov',
+    imgUrl: 'img/Cassie-Kozyrkov.jpg',
+    title: 'Consectetur sint, maxime adipisci fuga nobis quo!',
+    bio: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit dolort et uhuio',
   },
   {
     id: 3,
-    name: "Hakon Wium Lie",
-    imgUrl: "img/Hakon-Wium-Lie.jpg",
-    title: "Consectetur sint, maxime adipisci fuga nobis quo!",
-    bio: "Lorem, ipsum dolor sit amet consectetur adipisicing elit dolort et uhuio",
+    name: 'Hakon Wium Lie',
+    imgUrl: 'img/Hakon-Wium-Lie.jpg',
+    title: 'Consectetur sint, maxime adipisci fuga nobis quo!',
+    bio: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit dolort et uhuio',
   },
-]
+];
 
 const container = document.getElementById('speakers');
-speakers.forEach(speaker => {
+speakers.forEach((speaker) => {
   const content = `
     <div class="speaker-container">
       <div class="speaker-img">
